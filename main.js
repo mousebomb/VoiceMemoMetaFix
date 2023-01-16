d59c48bce3d4af6a1506f1190b2f9f4e194af6ed
Rhett Gao <mousebomb@gmail.com>
2023-01-16 18:19:46 +0800

    未来从导入目录转换

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 /** 对过去的批量文件全量处理
  * 新产生文件先拉到临时目录搞好了再来 */
-$inputFolder = "/Users/rhett/Documents/1-Life生活/生命历程/语音备忘录/2022/讲故事";
+$inputFolder = "/Users/rhett/Downloads/导入故事";
+$outputFolder= "/Users/rhett/Documents/1-Life生活/生命历程/语音备忘录/2023/讲故事";
 
 var fs = require("fs");
 var path = require("path");
@@ -34,7 +35,8 @@ var cmd ="";
 function fixMetadata(inputFullpath,filename)
 {
   let title = filename.split(".")[0];
-  let outFileName = inputFullpath.replace(filename,"new/"+filename);
+  // let outFileName = inputFullpath.replace(filename,"new/"+filename);
+  let outFileName = $outputFolder+"/"+filename;
   cmd+= `ffmpeg -i "${inputFullpath}" -metadata title="${title}" -metadata author="睿睿爸爸" -metadata album_artist="睿睿爸爸" -metadata album="睿睿爸爸讲故事" -c copy  "${outFileName}" -y\n`;
 }
 
